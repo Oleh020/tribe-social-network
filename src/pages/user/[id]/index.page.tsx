a9d5d44a377425a14c3fb3795d088d0999ab8ea1
Andrew <carbonid1@gmail.com>
2023-05-07 00:57:58 +0300

    qc

--- src/pages/user/[id]/index.page.tsx
+++ src/pages/user/[id]/index.page.tsx
@@ -1,8 +1,34 @@
-import { useRouter } from 'next/router';
+import { prisma } from '@/lib/prisma';
+import { User } from '@prisma/client';
+import { GetServerSideProps } from 'next';
 
-export default function UserPage() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return <div>User: {id}</div>;
+interface UserPage {
+  user: User
 }
+
+const UserPage: React.FC<UserPage> = ({ user }) => {
+
+  return <div>
+    <h1>{user.name}</h1>
+    <p>{user.id}</p>
+    <p>{user.email}</p>
+    <p>{user.createdAt.toString()}</p>
+    <p>{user.updatedAt.toString()}</p>
+  </div>;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({  params }) => {
+  const user = await prisma.user.findUnique({
+    where: {  id: String(params?.id)  },
+  });
+
+  if (!user) return {  notFound: true };
+
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(user)),
+    },
+  };
+};
+
+export default UserPage;
